c676fcf7ed4653d76ce9610e3c517866cab49ee6
salt-shaker <20408920+salt-shaker@users.noreply.github.com>
2021-02-23 22:38:42 -0800

    Working on toast component

--- exercise-tracker/src/components/popup-toast.component.js
+++ exercise-tracker/src/components/popup-toast.component.js
@@ -5,10 +5,41 @@ import Toast from 'react-bootstrap/Toast'
 const PopupToast = props => {
 const [showA, setShowA] = useState(true);
 const toggleShowA = () => setShowA(!showA);
+//const [toastDelay] = useState(2000);
+//const [toastCreated] = useState(new Date().getTime());
+//const [toastUpdated, setToastUpdated] = useState(new Date().getTime());
+//const [basicData, setBasicData] = useState(100);
 
+/*
+function updateTimer(){
+    setToastUpdated(new Date().getTime());
+    let timePassed = toastUpdated - toastCreated;
+    let timeRemaining;
+    if (timePassed >= 2000){
+        timeRemaining = 0;
+    } else {
+        timeRemaining = 2000 - timePassed;
+    }
+    setToastDelay(timeRemaining);
+}
+*/
+
+/*
+useEffect(() => {
+    setToastUpdated(new Date().getTime());
+    //setBasicData((toastDelay - (toastUpdated - toastCreated)))
+
+    if(toastDelay >= (toastUpdated - toastCreated)){
+        toggleShowA(); // NOT WORKING, useCallBack maybe?
+    }
+
+}, [toastCreated, toastDelay, toastUpdated, toggleShowA]);
+*/
+
+// delay={toastDelay >= (toastUpdated - toastCreated) ? (toastDelay - (toastUpdated - toastCreated)) : 0 }
     
 return React.createElement(() =>
-                <Toast key={props.data.keyid.toString()} show={showA} onClose={toggleShowA} delay={2000} animation={false} autohide>
+                <Toast key={props.data.keyid.toString()} show={showA} onClose={toggleShowA} animation={false} autohide>
                     <Toast.Header>
                         <img
                             src="holder.js/20x20?text=%20"
